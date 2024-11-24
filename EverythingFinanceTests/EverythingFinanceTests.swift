640878a32d3c84b10c32567c3f20169800c7c9d0
Harsh23Kashyap <harsh.kashyap2001@gmail.com>
2024-11-24 23:52:03 +0530

    Initial commit

---
+++ EverythingFinanceTests/EverythingFinanceTests.swift
@@ -0,0 +1,16 @@
+//
+//  EverythingFinanceTests.swift
+//  EverythingFinanceTests
+//
+//  Created by Harsh Kashyap on 10/29/24.
+//
+
+import Testing
+
+struct EverythingFinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
